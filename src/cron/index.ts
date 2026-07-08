94e379d8fa8d8fce5f4f7da2c9f75544c2f1a743
GDiiniiz <guilherme.diniz0056@gmail.com>
2026-07-08 14:14:32 -0300

    fix: ajuste horario cron

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { EnviarEmailCliente } from "../services/pedidos/enviarEmailCliente";
 
 export function startCronJobs() {
-  cron.schedule("* * * * *", () => {
+  cron.schedule("* 8-18 * * *", () => {
     EnviarEmailCliente.execute();
   });
 }
